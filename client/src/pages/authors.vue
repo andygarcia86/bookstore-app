236f8c474c2fe9089377dbdb62e9e7a1ca8fc9a1
Andy Garcia <andygarcia86@gmail.com>
2021-04-14 22:49:57 -0500

    Implement router

---
+++ client/src/pages/authors.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Authors</h1>
+    <p>
+      List of authors
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Authors',
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/pages/books.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Books</h1>
+    <p>
+      List of books
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Books',
+}
+</script>
+
+<style scoped>
+
+</style>
