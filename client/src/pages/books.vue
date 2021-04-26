be91041fb3f60e8164e85a2b4adb3cc67875a7c1
Andy Garcia <andygarcia86@gmail.com>
2021-04-25 19:57:27 -0500

    Add pagination

--- client/src/pages/books.vue
+++ client/src/pages/books.vue
@@ -3,6 +3,15 @@
     <h1>Books</h1>
     <b-button>Add Book</b-button>
     <b-table striped hover :items="books" :fields="fields"></b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      first-text="First"
+      prev-text="Prev"
+      next-text="Next"
+      last-text="Last"
+    ></b-pagination>
   </div>
 </template>
 
@@ -20,7 +29,10 @@ export default {
         { title: 'EVERYTHING I NEVER TOLD YOU', author: 'Larsen Shaw' },
         { title: 'IS EVERYONE HANGING OUT WITHOUT ME? (AND OTHER CONCERNS)', author: 'Geneva Wilson' },
         { title: 'The Electric Kool-Aid Acid Test ', author: 'Jami Carney' }
-      ]
+      ],
+      currentPage: 1,
+      rows: 214,
+      perPage: 10
     }
   }
 }
